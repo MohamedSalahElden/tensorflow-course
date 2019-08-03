dd7050302302a41d821e4a1a6df626d9192e6283
mohamed salah <mohamed.salah5369@gmail.com>
2019-08-04 01:33:51 +0200

    Update and rename NNtest.py to week1-fashion-classifier.py

--- week1-fashion-classifier.py
+++ week1-fashion-classifier.py
@@ -8,20 +8,16 @@ data = keras.datasets.fashion_mnist
 train_data = train_data / 255.0
 test_data = test_data /255.0
 
-
-
-model = keras.Sequential(
-	[
+model = keras.Sequential([
 	keras.layers.Flatten(input_shape = (28,28)),
 	keras.layers.Dense(units = 128 , activation = tf.nn.relu),
 	keras.layers.Dense(10 , activation = tf.nn.softmax)
-	]
-	)
+	])
 model.compile(optimizer = 'adam' , loss = 'sparse_categorical_crossentropy')
 
 model.fit(train_data , train_label , epochs = 5)
-
 model.evaluate(test_data , test_label)
+
 print(model.predict(test_data[20:50 , : , :])[10])
 print(test_label[30])
 
